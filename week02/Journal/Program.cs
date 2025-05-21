66c70a9185c273c85df6fd49762350acf3fa43a3
donovanpoland <dpoland@byupathway.edu>
2025-05-20 23:59:59 -0600

    Exceeding requirements comment

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -1,13 +1,22 @@
 using System;
-/*So I don't have to type console every time I need to use it.*/
-using static System.Console;
+
+
+
+/*Exceeding requirements: added an extra feature that keeps track of the number of entries
+the user enters into the journal, this number saves when you save your entry to the entry file
+in order to accomplish this I had to force the user to only save 1 entry at a time
+instead of int baches, in a real program entries would be saved automatically and I would
+not have to put this blocker on the number of entires at a time.
+
+there is also about 30 prompts, so a user could possibly get a new prompt every day for a month*/
+
 
 class Program
 {
     static void Main(string[] args)
     {
         Journal journal = new Journal();
-        
+
 
         while (journal._quit == false)
         {
@@ -15,5 +24,5 @@ class Program
         }
     }
 
-    
+
 }
