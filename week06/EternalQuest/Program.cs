d38bb4ab8d5a375784fd8c5dccce49607a274dbd
donovanpoland <dpoland@byupathway.edu>
2025-06-15 00:58:49 -0600

    exceed requirements comment

--- week06/EternalQuest/Program.cs
+++ week06/EternalQuest/Program.cs
@@ -1,6 +1,12 @@
 using System;
 using System.Drawing;
 
+/*
+Exceed requirements separated completed from uncompleted goals, added timers to make the menu more interactive.
+added back up method for file incase user forgets to save. 
+*/
+
+
 class Program
 {
     static void Main(string[] args)
